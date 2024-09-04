23850ed9bb83cbdec22c0056ddecbb9b2f5f6ad5
mayo-neko <by31536000sec@gmail.com>
2024-09-04 23:52:12 +0900

    Chapter1

---
+++ 1/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1.7 caffee break
+        System.out.println(calculateScore("imperative") == 9);
+        System.out.println(calculateScore("no") == 2);
+        System.err.println(wordScore("declarative") == 9);
+        System.err.println(wordScore("yes") == 3);
+    }
+    // 1.7 caffee break
+    public static int calculateScore(String word) {
+        int score = 0;
+        for (char c: word.toCharArray()) {
+            if (c == 'a') continue;
+            score++;
+        }
+        return score;
+    }
+    public static int wordScore(String word) {
+        return word.replace("a", "").length();
+    }
+}
---
+++ 1/Main.scala
@@ -0,0 +1,10 @@
+// 1.11 practice
+def increment(x: Int): Int = {
+    x+ 1
+}
+def getFirstCharacter(s: String): Char = {
+    s.charAt(0)
+}
+def wordScore(word: String): Int = {
+    word.length()
+}
